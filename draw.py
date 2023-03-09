0e2f7bf017fe3106f2c36a6409393777c6be5880
Serdar Yegulalp <serdar@infinimata.com>
2023-03-09 14:02:06 -0500

    possible optimizations

--- draw.py
+++ draw.py
@@ -1,4 +1,13 @@
+# cython: boundscheck=False
+# cython: wraparound=False
+
 from random import randint, choice
+import cython
+
+if cython.compiled:
+    from cython.cimports.cpython import array as arr  # type: ignore
+else:
+    import array as arr
 
 
 def event(self, *a):
@@ -26,7 +35,7 @@ def event(self, *a):
 
     moves: list = []
     eats: list = []
-    b = self.buffer
+    b: arr.array = self.buffer
 
     clear_moves = moves.clear
     clear_eats = eats.clear
@@ -34,7 +43,9 @@ def event(self, *a):
     append_eats = eats.append
 
     c1 = self.colors[1]
+    # c1: cython.uchar[4] = (255, 32, 32, 255)
     c2 = self.colors[2]
+    # c2: cython.uchar[4] = (0, 255, 0, 255)
 
     ch = choice
     ri = randint
@@ -44,70 +55,64 @@ def event(self, *a):
 
     seq: list = self.seq
 
-    # TODO: store offset computations
-
     for pos in seq:
         repro = fish_repro[pos]
-        if not repro:
-            continue
+        if repro:
+            repro += 1
 
-        repro += 1
-
-        clear_moves()
-        for offset in offsets[pos]:
-            if fish_repro[offset] == 0 and other_fish_repro[offset] == 0:
-                append_moves(offset)
-        if moves:
-            target_move = ch(moves)
-            if repro >= fish_repro_time:
-                other_fish_repro[pos] = ri(1, fish_repro_time)
-                other_fish_repro[target_move] = ri(1, fish_repro_time)
+            clear_moves()
+            for offset in offsets[pos]:
+                if fish_repro[offset] == 0 and other_fish_repro[offset] == 0:
+                    append_moves(offset)
+            if moves:
+                target_move = ch(moves)
+                if repro >= fish_repro_time:
+                    other_fish_repro[pos] = ri(1, fish_repro_time)
+                    other_fish_repro[target_move] = ri(1, fish_repro_time)
+                else:
+                    other_fish_repro[target_move] = repro
             else:
-                other_fish_repro[target_move] = repro
-        else:
-            other_fish_repro[pos] = repro
-        fish_repro[pos] = 0
+                other_fish_repro[pos] = repro
+            fish_repro[pos] = 0
 
-        b[pos * 4 : pos * 4 + 4] = c2
+            b[pos * 4 : pos * 4 + 4] = c2[:]
 
-    for pos in seq:
         repro = shark_repro[pos]
-        if not repro:
-            continue
-        repro += 1
-        life = shark_life[pos] + 1
-        if life > shark_starves:
-            shark_repro[pos] = 0
-            continue
+        if repro:
+            repro += 1
+            life = shark_life[pos] + 1
+            if life > shark_starves:
+                shark_repro[pos] = 0
+                continue
 
-        clear_moves()
-        clear_eats()
-        for offset in offsets[pos]:
-            if other_fish_repro[offset] != 0:
-                append_eats(offset)
-            if shark_repro[offset] == 0 and other_shark_repro[offset] == 0:
-                append_moves(offset)
-        if eats:
-            target_move = ch(eats)
-            other_shark_repro[target_move] = repro
-            other_shark_life[target_move] = 1
-            other_fish_repro[target_move] = 0
-        elif moves:
-            target_move = ch(moves)
-            if repro > shark_repro_time:
-                other_shark_repro[pos] = 1
-                other_shark_life[pos] = 1
-                other_shark_life[target_move] = 1
-                other_shark_repro[target_move] = 1
-            else:
-                other_shark_life[target_move] = life
+            clear_moves()
+            clear_eats()
+            for offset in offsets[pos]:
+                if other_fish_repro[offset] != 0:
+                    append_eats(offset)
+                if shark_repro[offset] == 0 and other_shark_repro[offset] == 0:
+                    append_moves(offset)
+            if eats:
+                target_move = ch(eats)
                 other_shark_repro[target_move] = repro
+                other_shark_life[target_move] = 1
+                other_fish_repro[target_move] = 0
+            elif moves:
+                target_move = ch(moves)
+                if repro > shark_repro_time:
+                    other_shark_repro[pos] = 1
+                    other_shark_life[pos] = 1
+                    other_shark_life[target_move] = 1
+                    other_shark_repro[target_move] = 1
+                else:
+                    other_shark_life[target_move] = life
+                    other_shark_repro[target_move] = repro
 
-        else:
-            other_shark_repro[pos] = repro
-            other_shark_life[life] = life
-        shark_repro[pos] = 0
+            else:
+                other_shark_repro[pos] = repro
+                other_shark_life[life] = life
+            shark_repro[pos] = 0
 
-        b[pos * 4 : pos * 4 + 4] = c1
+            b[pos * 4 : pos * 4 + 4] = c1[:]
 
     self.world = not self.world
